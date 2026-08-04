ae863dd1be31115a907fc2beb8f46c273a92ed0f
sksakibboss2820-dotcom <sksakibboss2820@gmail.com>
2026-08-05 00:04:13 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,24 @@
-
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-from config import BOT_TOKEN
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "🔥 Welcome to ST Wingo Master 🔥"
     )
 
-app = Application.builder().token(BOT_TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
 
-print("Bot Started...")
-app.run_polling()
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot Started...")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
